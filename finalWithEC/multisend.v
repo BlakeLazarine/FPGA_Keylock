898479313381ad990f9286a532e47eace740818c
Blake Lazarine <blake.l.lazarine@gmail.com>
2020-06-09 19:34:54 -0700

    semi-semi-functional multichar display

--- finalWithEC/multisend.v
+++ finalWithEC/multisend.v
@@ -10,7 +10,7 @@ module multisend (hwclk, num, enabled, out0, out1, out2, controlOut, done);
     /* Counter register */
     reg prevEnabled = 0;
     reg complete = 0;
-    assign done = complete;
+    assign done = completed;
 
 
     reg [3:0] ch = 0;
@@ -26,52 +26,66 @@ module multisend (hwclk, num, enabled, out0, out1, out2, controlOut, done);
 
     reg rdy = 0;
     reg prevDoneSending = 0;
+
+    reg [31:0] num10 = 0;
+    reg [31:0] num100 = 0;
+    reg [31:0] num1000 = 0;
+    reg [31:0] num10000 = 0;
+    reg [31:0] num100000 = 0;
+    reg [31:0] num1000000 = 0;
+
     always @ (posedge hwclk) begin
         if(enabled) begin
             if(!prevEnabled) begin
+                num10 = num%10;
+		num100 = num%100;
+		num1000 = num%1000;
+		num10000 = num%10000;
+		num100000 = num%100000;
+		num1000000 = num%1000000;
+                
+		numbers[15] = num10[0];
+                numbers[16] = num10[1];
+                numbers[17] = num10[2];
 
-                numbers[15] = (num % 10)[0];
-                numbers[16] = (num % 10)[1];
-                numbers[17] = (num % 10)[2];
+                numbers[12] = num100[0];
+                numbers[13] = num100[1];
+                numbers[14] = num100[2];
 
-                numbers[12] = (num % 100)[0];
-                numbers[13] = (num % 100)[1];
-                numbers[14] = (num % 100)[2];
+                numbers[9] = num1000[0];
+                numbers[10] = num1000[1];
+                numbers[11] = num1000[2];
 
-                numbers[9] = (num % 1000)[0];
-                numbers[10] = (num % 1000)[1];
-                numbers[11] = (num % 1000)[2];
+                numbers[6] = num10000[0];
+                numbers[7] = num10000[1];
+                numbers[8] = num10000[2];
 
-                numbers[6] = (num % 10000)[0];
-                numbers[7] = (num % 10000)[1];
-                numbers[8] = (num % 10000)[2];
+                numbers[3] = num100000[0];
+                numbers[4] = num100000[1];
+                numbers[5] = num100000[2];
 
-                numbers[3] = (num % 100000)[0];
-                numbers[4] = (num % 100000)[1];
-                numbers[5] = (num % 100000)[2];
+                numbers[0] = num1000000[0];
+                numbers[1] = num1000000[1];
+                numbers[2] = num1000000[2];
 
-                numbers[0] = (num % 1000000)[0];
-                numbers[1] = (num % 1000000)[1];
-                numbers[2] = (num % 1000000)[2];
-
-                i <= 0;
-                ch <= 0;
-                rdy <= 1;
-                complete <= 0;
+                i = 0;
+                ch = 0;
+                rdy = 1;
+                completed = 0;
             end
             else if(!completed)
                 rdy = prevDoneSending;
 
             enableSender = (rdy) ? 1 : !doneSending;
             if(doneSending & !completed) begin
-                ch[0] = numbers[3 * i];
-                ch[1] = numbers[3 * i + 1];
-                ch[2] = numbers[3 * i + 2];
+                ch[0] <= numbers[3 * i];
+                ch[1] <= numbers[3 * i + 1];
+                ch[2] <= numbers[3 * i + 2];
 
                 i <= i+1;
             end
-            if(i == 9)
-                completed = 1;
+            if(i == 5)
+                completed <= 1;
 
         end
         prevDoneSending = doneSending;
