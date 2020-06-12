3368a7711dc1a4f47b3bdc02f6ccaef24295f1a9
Blake Lazarine <blake.l.lazarine@gmail.com>
2020-06-11 17:24:16 -0700

    forgot some

--- TestBenches/multisendTB.v
+++ TestBenches/multisendTB.v
@@ -6,57 +6,55 @@
 
 module ha2_task2_tb();
   reg hwclk, enabled = 0;
-  reg done, active, out0, out1, out2, controlOut;
-  reg [3:0] num = 0;
+  reg done, out0, out1, out2, controlOut;
+  reg [31:0] num = 555116;
   
 
   integer i, j;
-  sender senderTB(.hwclk(hwclk), .num(num), .enabled(enabled), .done(done), .out0(out0), .out1(out1), .out2(out2), .controlOut(controlOut), .active(active));
+  multisend multisendTB(.hwclk(hwclk), .num(num), .enabled(enabled), .out0(out0), .out1(out1), .out2(out2), .controlOut(controlOut), .done(done));
   
   //go through increasingly large pulse width
   initial begin
     $dumpfile("dump.vcd"); $dumpvars;
-    for(num = 0; num < 10; num = num + 1) begin
-      #8
-      hwclk = 0;
-      $display("num %d", num);
-      #8
-      hwclk = 1;
-      #8
-      hwclk = 0;
-      #8
-      hwclk = 1;
-      #8
-      hwclk = 0;
-      #8
-      hwclk = 1;
-      #8
-      hwclk = 0;
-      #4
-      enabled = 1;
-      #4
-      hwclk = 1;
-      #8
-      hwclk = 0;
-      while(active) begin
-        #8
-        hwclk = 1;
-        #8
-        hwclk = 0;
-      end
-      enabled = 0;
-      #8
-      hwclk = 1;
-      #8
-      hwclk = 0;
-      #8
-      hwclk = 1;
-      #8
-      hwclk = 0;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #4
+    enabled = 1;
+    #4
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    while(!done) begin
       #8
       hwclk = 1;
       #8
       hwclk = 0;
     end
+    enabled = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+  
   end
 endmodule
--- TestBenches/patternTB.v
+++ TestBenches/patternTB.v
@@ -5,34 +5,55 @@
 `timescale 1ns / 1ps
 
 module ha2_task2_tb();
-  reg CLK, RST, DIN;
-  wire DOUT_SHORT, DOUT_LONG;
+  reg hwclk, enable = 0;
+  reg done, bright;
+  reg [31:0] ontime = 3;
+  reg [31:0] offtime = 2;
+  reg [7:0] reps = 3;
+
   integer i, j;
-  ha2_task2 ha2_task2_tb(.CLK(CLK),.DIN(DIN),.RST(RST),.DOUT_SHORT(DOUT_SHORT), .DOUT_LONG(DOUT_LONG));
+  pattern pat(.hwclk(hwclk), .ontime(ontime), .offtime(offtime), .reps(reps), .done(done), .enable(enable), .bright(bright));
   
   //go through increasingly large pulse width
   initial begin
-    for (i=6'b1;i<12;i=i+1) begin
+    $dumpfile("dump.vcd"); $dumpvars;
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #4
+    enable = 1;
+    #4
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    while(!done) begin
       #8
-      RST = 1;
+      hwclk = 1;
       #8
-      CLK = 0;
-      #8
-      CLK = 1;
-      #8
-      RST = 0;
-      CLK = 0;
-      //have DIN be high for i clock cycles and low for i clock cycles. Reapting
-      for(j=6'b0;j<128;j=j+1) begin
-        #8
-        DIN = (j / (2 * i)) % 2;
-        #8
-        CLK = j % 2;
-        #8
-        if(CLK)
-          $display("i %d DIN %b SHORT %b LONG %b", i, DIN, DOUT_SHORT, DOUT_LONG);
-        
-      end
+      hwclk = 0;
     end
+    enable = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
+    #8
+    hwclk = 1;
+    #8
+    hwclk = 0;
   end
 endmodule
